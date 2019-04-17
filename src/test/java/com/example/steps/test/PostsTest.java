111535978a8bd608c6b7ac25d234da7750388ea1
marciovrl <contato.marciovrl@gmail.com>
2019-04-16 21:19:10 -0300

    Initial commit

---
+++ src/test/java/com/example/steps/test/PostsTest.java
@@ -0,0 +1,61 @@
+package com.example.steps.test;
+
+import io.restassured.RestAssured;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+public class PostsTest {
+    
+    @BeforeClass
+    public static void setUp() {
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com/posts";
+    }
+
+	@Test
+    public void validateGetReturn() {
+        given().
+            contentType("application/json").
+        when().
+            get("/").
+        then().
+            statusCode(200);
+    }
+
+    @Test
+    public void validatePostReturn() {
+        given().
+            contentType("application/json").
+            body("{\"title\":\"Testing\", \"body\":\"Testing API example\"}").
+        when().
+            post("/").
+        then().
+            statusCode(201).
+            body("title", is("Testing"));
+    }
+
+    @Test
+    public void validatePutReturn() {
+        given().
+            contentType("application/json").
+            body("{\"title\":\"New Testing\"}").
+        when().
+            put("/1").
+        then().
+            statusCode(200).
+            body("title", is("New Testing"));
+    }
+
+    @Test
+    public void validateDeleteReturn() {
+        given().
+            contentType("application/json").
+        when().
+            delete("/1").
+        then().
+            statusCode(200);
+    }
+
+}
