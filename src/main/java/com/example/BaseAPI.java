fdbe8a3c995cb7b099afa7e56513122a8eafbe58
marcio lemos <contato.marciovrl@gmail.com>
2020-04-22 18:32:14 -0300

    created BaseAPI

---
+++ src/main/java/com/example/BaseAPI.java
@@ -0,0 +1,11 @@
+package com.example;
+
+/**
+ * Constants with values ​​for RestAssured configuration
+ *
+ */
+public interface BaseAPI {
+    String BASE_URI = "https://jsonplaceholder.typicode.com";
+    String BASE_PATH = "";
+    Long MAX_TIMEOUT = 90000L;
+}
